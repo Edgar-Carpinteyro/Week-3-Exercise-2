85ff4399397adf9df0ef8ade93c20bba1db1ad95
Edgar-Carpinteyro <carpinteyroe07@gmail.com>
2023-10-16 21:44:41 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,48 @@
+function GiveMeResult() {
+    let firstOperand = Number(document.getElementById("firstOperand").value);
+    let operator = document.getElementById("operators").value;
+    let secondOperand = Number(document.getElementById("secondOperand").value);
+  
+    let resultSpan = document.getElementById("result");
+    let resultNum = null;
+  
+    switch(operator) {
+        case "+":
+          resultNum = firstOperand + secondOperand;
+          resultSpan.innerHTML = resultNum;
+          break;
+        case "-":
+          resultNum = firstOperand - secondOperand;
+          resultSpan.innerHTML = resultNum;
+          break;
+        case "*":
+            resultNum = firstOperand * secondOperand;
+            resultSpan.innerHTML = resultNum;
+            break;
+        case "/":
+            resultNum = firstOperand / secondOperand;
+            resultSpan.innerHTML = resultNum;
+            break;
+        case "**":
+            resultNum = firstOperand ** secondOperand;
+            resultSpan.innerHTML = resultNum;
+            break;
+        case "%":
+            resultNum = firstOperand % secondOperand;
+            resultSpan.innerHTML = resultNum;
+            break;
+            
+        case ">":
+            if(firstOperand > secondOperand) {
+            resultSpan.innerHTML = "True";
+            } else {
+            resultSpan.innerHTML = "False";
+            }
+            break;
+        default:
+            alert("No operator found!")
+        }
+  }
+  
+  let button = document.getElementById("button");
+  button.addEventListener("click", GiveMeResult);
